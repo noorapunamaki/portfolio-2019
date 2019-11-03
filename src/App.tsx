fdfa9cbf6927a7769d5b928ecbbc5fc4c42681ef
Noora Pirttilahti <noora@pirttilahti.net>
2019-11-03 11:11:06 +0200

    app customization

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () =>
+  <>
+    <header>
+      <h1>Noora Pirttilahti</h1>
+    </header>
+    <main>
+      Content
+    </main>
+    <footer>
+      footer
+    </footer>
+  </>;
 
 export default App;
